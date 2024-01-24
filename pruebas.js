49687a838f218d6cc82d83ea3cf0cff25fe6786c
nelferna2023 <136538241+nelferna2023@users.noreply.github.com>
2024-01-24 09:59:50 -0500

    ultima versión del juego

---
+++ pruebas.js
@@ -0,0 +1,19 @@
+let numeros=['mama','papa','hijo'];
+let listaVacia=[];
+//console.log (numeros);
+
+for(let i= numeros.length-1 ; i>=0 ;i--){
+    //console.log(i);
+    //console.log(numeros[i]);
+    listaVacia.push(numeros[i]);
+
+}
+console.log(numeros);
+console.log(listaVacia);
+/*function mostrarLenguagesReversoSeparadamente() {
+    for (let i = lenguagesDeProgramacion.length - 1; i >= 0; i--) {
+      console.log(lenguagesDeProgramacion[i]);
+    }
+  }
+  */
+
